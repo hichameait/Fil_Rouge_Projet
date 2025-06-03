89bac7e99b7a62b046fa052c51c0159c4dd28fa8
hichameait <its.aitbenalla.hichame@gmail.com>
2025-06-03 13:20:13 +0100

    finished hero section

--- index-2.php
+++ index-2.php
@@ -25,15 +25,27 @@
     <main>
         <div class="hero">
             <p class="header_para">
-                La gestion dentaire moderne commence ici <img src="https://cdn.animaapp.com/projects/683cb56bcb1fa081be2f8b2c/releases/683cb689aa63b0519c365b09/img/component-1-3.svg" alt="" width="20px" height="20px">
+                La gestion dentaire moderne commence ici <img
+                    src="https://cdn.animaapp.com/projects/683cb56bcb1fa081be2f8b2c/releases/683cb689aa63b0519c365b09/img/component-1-3.svg"
+                    alt="" width="20px" height="20px">
             </p>
             <h1 id="hero-title">Une solution digitale complète
-pour les dentistes marocains</h1>
+                pour les dentistes marocains</h1>
             <p id="hero-subtitle">Rendez-vous, paiements, patients et documents…
-Tout en un seul outil 100% digital.</p>
-
+                Tout en un seul outil 100% digital.</p>
+            <div class="btn-groups">
+                <button id="nav-btn" >Essai Gratuit 7 Jours</button>
+                <button id="btn-prix" >Prix <img
+                    src="https://cdn.animaapp.com/projects/683cb56bcb1fa081be2f8b2c/releases/683cb689aa63b0519c365b09/img/component-1-3.svg"
+                    alt="" width="20px" height="20px"></button>
+            </div>
+            <p id="subtitle-small">Votre cabinet, votre rythme — notre technologie</p>
+            <br>
+            <hr id="dev_line">
+            <img src="./assets/dash.svg" alt="" srcset="">
         </div>
     </main>
+
 </body>
 
 </html>
